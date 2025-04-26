dd1b1c9e495ef8bfbe8db759e57eb299cc6b5e04
CODER-AMLAN <145832307+CODER-AMLAN@users.noreply.github.com>
2025-04-26 15:56:26 +0530

    Update H5.java

--- Home Assignment/Assignment3/H5.java
+++ Home Assignment/Assignment3/H5.java
@@ -1,6 +1,6 @@
 package Assignment3;
 import java.util.Scanner;
-public class P5 {
+public class H5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number: ");
