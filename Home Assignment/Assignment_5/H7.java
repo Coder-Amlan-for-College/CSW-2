9523b2bf5eb61c08daece673f5a8d7a13acdd21e
CODER-AMLAN <145832307+CODER-AMLAN@users.noreply.github.com>
2025-04-28 22:19:00 +0530

    Update H7.java

--- Home Assignment/Assignment_5/H7.java
+++ Home Assignment/Assignment_5/H7.java
@@ -1,28 +1,39 @@
 package Assignment_5;
-import java.util.*;
+import java.util.Scanner;
 public class H7 {
-	public static void main(String[] args) {
-         Scanner sc = new Scanner(System.in);
-		 System.out.print("Enter the sentence: ");
-		 String sentence = sc.nextLine();
-		 System.out.print("Enter the word to search: ");
-		 String searchWord = sc.next().trim();
-		 System.out.print("Enter the word to replace with: ");
-		 String replaceWord = sc.next().trim();
-		 if(sentence.toLowerCase().contains(searchWord.toLowerCase())){
-		     System.out.println("Original sentence: "+sentence);
-			 sentence = sentence.replaceAll("(?i)"+searchWord, replaceWord);
-			 System.out.println("Modified sentence: "+sentence);}
-		 else{
-			System.out.println("The word '"+searchWord+ "' does not exist in the sentence.");
-		 }
-      sc.close();		     
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the sentence: ");
+        String sentence = sc.nextLine().trim();
+        sentence = sentence.concat(" ");
+        System.out.println("Enter the word to search: ");
+        String searchWord = sc.next().trim();
+        System.out.println("Enter the replacement word: ");
+        sc.nextLine();
+        String replaceWord = sc.nextLine().trim();
+        String word="";int startIndex = 0; String modified = "";
+        for(int i=0;i<sentence.length();i++){
+            char ch = sentence.charAt(i);
+            if(Character.isWhitespace(ch)){
+                int lastIndex = i;
+                word = word.trim();
+                if(word.equalsIgnoreCase(searchWord)){
+                    String sub1 = sentence.substring(0,startIndex);
+                    String sub2 = sentence.substring(lastIndex);
+                     modified = sub1.concat(replaceWord).concat(sub2);
+                    break;
+                }
+                word = "";
+                startIndex = i+1;
+            }
+            word = word+ch;
+        }
+        System.out.println("Original: "+sentence);
+        if(modified.isEmpty()){
+            System.out.println("Search word doesnot exist no modification!");
+        }
+        else {
+            System.out.println("Modified: " + modified.trim());
+        }
+    }
 }
-
-// OUTPUT
-// Enter the sentence: Hello everyone
-// Enter the word to search: Everyone
-// Enter the word to replace with: World
-// Original sentence: Hello everyone
-// Modified sentence: Hello World   
