ac466f2d61adc21eaaddb864d1c1fafd131af5fc
CODER-AMLAN <145832307+CODER-AMLAN@users.noreply.github.com>
2025-04-29 22:38:14 +0530

    Update H7.java

--- Home Assignment/Assignment_5/H7.java
+++ Home Assignment/Assignment_5/H7.java
@@ -5,35 +5,23 @@ public class H7 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the sentence: ");
         String sentence = sc.nextLine().trim();
-        sentence = sentence.concat(" ");
         System.out.println("Enter the word to search: ");
         String searchWord = sc.next().trim();
         System.out.println("Enter the replacement word: ");
         sc.nextLine();
         String replaceWord = sc.next().trim();
-        String word="";int startIndex = 0; String modified = "";
-        for(int i=0;i<sentence.length();i++){
-            char ch = sentence.charAt(i);
-            if(Character.isWhitespace(ch)){
-                int lastIndex = i;
-                word = word.trim();
-                if(word.equalsIgnoreCase(searchWord)){
-                    String sub1 = sentence.substring(0,startIndex);
-                    String sub2 = sentence.substring(lastIndex);
-                     modified = sub1.concat(replaceWord).concat(sub2);
-                    break;
-                }
-                word = "";
-                startIndex = i+1;
-            }
-            word = word+ch;
-        }
-        System.out.println("Original: "+sentence);
-        if(modified.isEmpty()){
-            System.out.println("Search word doesnot exist no modification!");
+        String modified = "";
+        int index = sentence.toLowerCase().indexOf(searchWord.toLowerCase());
+        if(index != -1){
+            String s1 = sentence.substring(0,index);
+            String s2 = sentence.substring(index+searchWord.length());
+            modified = s1.concat(replaceWord).concat(s2);
         }
         else {
-            System.out.println("Modified: " + modified.trim());
+            System.out.println("Word not found. No modification Exiting!");
+            System.exit(0);
         }
+        System.out.println("Original: "+sentence);
+        System.out.println("Modified: "+modified);
     }
 }
