652c5d17caac1421a0d67257acdd85a7617c85d5
CODER-AMLAN <145832307+CODER-AMLAN@users.noreply.github.com>
2025-05-08 22:41:14 +0530

    Update Q4.java

--- Home Assignment/Assignment_7/Q4.java
+++ Home Assignment/Assignment_7/Q4.java
@@ -6,7 +6,55 @@ default method to print the area of each shape.*/
 
 package Assignment_7;
 
-
-public class Q4 {
-
+import java.util.Scanner;
+@FunctionalInterface
+interface Shape
+{
+    double area();
+    default void printArea(){
+        System.out.println("Area: "+area());
+    }
 }
+public class H7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Shape circle = ()->{
+            System.out.print("Enter the radius: ");
+            double r = sc.nextDouble();
+            return r*r;
+        };
+        Shape square = ()->{
+            System.out.print("Enter the side: ");
+            double s = sc.nextDouble();
+            return s*s;
+        };
+        Shape rectangle = ()->{
+            System.out.print("Enter the length: ");
+            double l = sc.nextDouble();
+            System.out.print("Enter the breadth: ");
+            double b = sc.nextDouble();
+            return l*b;
+        };
+        System.out.println("Circle: ");
+        circle.printArea();
+        System.out.println("\nSquare: ");
+        square.printArea();
+        System.out.println("\nRectangle: ");
+        rectangle.printArea();
+    }
+}
+
+/*OUTPUT
+Circle: 
+Enter the radius: 5
+Area: 25.0
+
+Square: 
+Enter the side: 4
+Area: 16.0
+
+Rectangle: 
+Enter the length: 4
+Enter the breadth: 3
+Area: 12.0OUTPUT
+*/
